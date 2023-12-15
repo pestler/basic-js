d36d730b3ee5eeba7fa66951c9556d43f1d09afd
Aleh Kuis <pestleralex@gmail.com>
2023-12-15 11:10:05 +0300

    feat: start

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  if ( typeof(sampleActivity) != "string" 
+  || !Number(sampleActivity)
+  || (sampleActivity <= 0 
+  || sampleActivity > MODERN_ACTIVITY)) return false;
+  return Math.ceil(Math.log10(MODERN_ACTIVITY/sampleActivity)/(Math.log10(2)/HALF_LIFE_PERIOD))
+};
 
 module.exports = {
   dateSample
